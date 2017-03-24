a4cd72fda0f8af9b9deab17131bf922c2605afa0
Owen <1787hz@gmail.com>
2017-03-24 13:39:17 +0800

    hw3

---
+++ 24_hw1/www/scripts/app.bootstrap.js
@@ -0,0 +1,7 @@
+angular.element(document).ready(function () {
+    angular.bootstrap(document, ['2017Apps']);
+});
+
+//angular.element(function () {
+//    angular.bootstrap(document, ['2017Apps']);
+//});
