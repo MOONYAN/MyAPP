07370451818c7c2bab71863f1e46d6d067495188
Owen <1787hz@gmail.com>
2017-04-18 08:08:57 +0800

    hw5

--- 24_hw1/www/scripts/app.run.js
+++ 24_hw1/www/scripts/app.run.js
@@ -1,1 +1,4 @@
-﻿angular.module('2017Apps').run();
+﻿angular.module('2017Apps').run(['$rootScope', function ($rootScope) {
+    //$rootScope.iStoreUrl = 'http://127.0.0.1:3024/apps24/istore';
+    $rootScope.iStoreUrl = 'http://104.199.219.156:8080/apps24/istore';
+}]);
